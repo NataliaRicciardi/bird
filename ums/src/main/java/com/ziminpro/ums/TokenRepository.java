39b6560be188a4198bf49b7d91089a3ec9c4a833
NataliaR <natric.serafina@gmail.com>
2024-12-07 02:23:49 -0500

    print tokens and user info

--- ums/src/main/java/com/ziminpro/ums/TokenRepository.java
+++ ums/src/main/java/com/ziminpro/ums/TokenRepository.java
@@ -2,7 +2,12 @@ package com.ziminpro.ums;
 
 import com.ziminpro.ums.dtos.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TokenRepository extends JpaRepository<Token, String> {
     Token findByToken(String token);
+
+    @Query(value = "SELECT * FROM ums.tokens t WHERE t.github_user_id = :githubUserId ORDER BY t.expiration_time DESC LIMIT 1", nativeQuery = true)
+    Token findByGithubUserId(@Param("githubUserId") String githubUserId);
 }
--- ums/src/main/java/com/ziminpro/ums/controllers/HomeController.java
+++ ums/src/main/java/com/ziminpro/ums/controllers/HomeController.java
@@ -1,13 +1,47 @@
 package com.ziminpro.ums.controllers;
 
+import com.ziminpro.ums.TokenRepository;
+import com.ziminpro.ums.dtos.Token;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final TokenRepository tokenRepository;
+
+    public HomeController(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
     @GetMapping("/")
     public String home() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof OAuth2User) {
+            OAuth2User oAuth2User = (OAuth2User) principal;
+
+            Object githubUserIdObject = oAuth2User.getAttribute("id");
+            String githubUserId = (githubUserIdObject instanceof Integer)
+                    ? String.valueOf(githubUserIdObject)
+                    : githubUserIdObject != null ? githubUserIdObject.toString() : "Unknown";
+
+            Token token = tokenRepository.findByGithubUserId(githubUserId);
+
+            if (token != null) {
+                return String.format(
+                        "Welcome, %s!<br>Email: %s<br>Your Token: %s",
+                        token.getName(),
+                        token.getEmail(),
+                        token.getToken()
+                );
+            } else {
+                return "Welcome! We could not retrieve your token.";
+            }
+        }
+
         return "Welcome to the application!";
     }
 
