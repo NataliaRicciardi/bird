90916d16b5716074c01d6a73a4199d7307062c0d
NataliaR <natric.serafina@gmail.com>
2024-12-07 21:33:29 -0500

    save

--- ums/src/main/java/com/ziminpro/ums/dtos/User.java
+++ ums/src/main/java/com/ziminpro/ums/dtos/User.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User { // change to save
     UUID id;
     String name;
     String email;
